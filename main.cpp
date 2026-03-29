e1a7de38a133b0619b4bff7f64746c07463eae9f
Kotobukiyama <131399027+Kotobukiyama@users.noreply.github.com>
2026-03-29 18:18:15 -0300

    Simple sum added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello world!" << '\n';
+    std::cout << "Choose two integers" << '\n';
+
+    int x{};
+    std::cin >> x;
+
+    int y{};
+    std::cin >> y;
+
+    std::cout << "The sum of " << x << " and " << y << " is " << x + y << '\n';
+
     return 0;
 }
